2cf8022d61483b6302a5efda822c66842c08035d
Moldovan Cristian <christi_nicolae@yahoo.com>
2024-02-05 15:55:10 +0200

    connect to git

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*Scanner scanner = new Scanner(System.in);
+
+	System.out.println("what is you name? ");
+	String name = scanner.nextLine();
+	System.out.println("How old are you? ");
+	int age = scanner.nextInt();
+	scanner.nextLine();
+	//String age = scanner.nextLine();
+	System.out.println("What is you fav food? ");
+	String food = scanner.nextLine();
+	System.out.println("Hello " +name);
+	System.out.println("You are " +age+ " years old!");
+	System.out.println("You like " +food);*/
+	String[] numeMatrice = {"Cluj", "Buc"};
+	for (String nume : numeMatrice) {
+		System.out.println(nume);
+	}
+
+    }
+}
